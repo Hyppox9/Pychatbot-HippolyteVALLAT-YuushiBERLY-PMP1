aee13e0453e841f76f473570dcc69947c5ebdb62
Yuushi000 <yuushi.berly@efrei.net>
2023-12-01 09:59:16 +0100

    pourrais tu m'accepter? stp

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print("hello world")
-for i in range(5):
-    print(i)
+import os
+
+
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+
+# Call of the function
+directory = "./speeches"
+files_names = list_of_files(directory, "txt")
+print_list(files_names)
+
+mintxt = "haha"
+print(mintxt)

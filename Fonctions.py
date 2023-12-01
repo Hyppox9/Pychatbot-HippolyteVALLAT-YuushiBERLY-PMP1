fbb828a3e86171de23b259f7dabffc352a5262bf
Hyppox9 <hippolytevallat@gmail.com>
2023-12-01 09:56:59 +0100

    test

--- Fonctions.py
+++ Fonctions.py
@@ -1,9 +1,9 @@
 import os
+import math
 
 def extract_names(repertoire):
     L = []
     for fichier in os.listdir(repertoire):
-
         if "1" in fichier and fichier.removeprefix("Nomination_").removesuffix("1.txt") not in L:
             L.append(fichier.removeprefix("Nomination_").removesuffix("1.txt"))
         elif "2" in fichier and fichier.removeprefix("Nomination_").removesuffix("2.txt") not in L:
@@ -11,7 +11,20 @@ def extract_names(repertoire):
         elif "1" not in fichier and "2" not in fichier and fichier.removeprefix("Nomination_").removesuffix(
                 ".txt") not in L:
             L.append(fichier.removeprefix("Nomination_").removesuffix(".txt"))
-    for e in L:
-        print(e)
+    L1 : list.str = []
+    for i in range(len(L)):
+        if L[i] not in L1:
+            L1.append(L[i])
+    return L1
 
-extract_names("./speeches")
+
+
+diconames = {"Macron" : "Emmanuel Macron",
+             "Chirac" : "Jacques Chirac",
+             "Giscard dEstaing" : "Valerie Giscard dEstaing",
+             "Hollande" : "Francois Hollande",
+             "Mitterrand" : "Francois Mitterrand",
+             "Sarkozy" : "Nicolas Sarkozy"}
+
+
+

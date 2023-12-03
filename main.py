a76c6ebeafece32ea5d9b1d8427fb43e9ef4128b
Yuushi000 <yuushi.berly@efrei.net>
2023-12-03 15:27:09 +0100

    pour n'ettoyer les fichiers

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ def propre(repertoire):
     for fichier in os.listdir(repertoire):
         if fichier.endswith(".txt"):
             fichiers.append(fichier)
-
+###crée une fonction qui permettra de lire directement a partir des fichier
     for j in range(len(fichiers)):
 
-        L1 = []
+        txt1 = []
 
         file = open("{}\\{}".format(repertoire, fichiers[j]), "r", encoding="utf8")
         lines = file.readlines()
         for n, line in enumerate(lines):
-            L1.append(str(line).replace("\n", " "))
+            txt1.append(str(line).replace("\n", " "))
         file.close()
         for i in range(len(L1)):
-            L1[i] = L1[i].replace("'", " ")
-            L1[i] = L1[i].replace(".", "")
-            L1[i] = L1[i].replace(",", "")
-            L1[i] = L1[i].replace(";", "")
-            L1[i] = L1[i].replace("!", "")
-            L1[i] = L1[i].replace("?", "")
-            L1[i] = L1[i].replace("-", " ")
-            L1[i] = L1[i].lower()
-
+            txt1[i] = txt1[i].replace(".", " ")
+            txt1[i] = txt1[i].replace(",", "")
+            txt1[i] = txt1[i].replace("'", "")
+            txt1[i] = txt1[i].replace(";", "")
+            txt1[i] = txt1[i].replace("!", "")
+            txt1[i] = txt1[i].replace("?", "")
+            txt1[i] = txt1[i].replace("-", " ")
+            txt1[i] = txt1[i].lower()
+###permet de remplacer tout les points, virgule etc par des espaces et de tout mettre en minuscule
         f = open("{}\\{}".format(repertoire, fichiers[j]), "w")
         for n, line in enumerate(lines):
-            f.write("{}\n".format(L1[n]))
+            f.write("{}\n".format(txt1[n]))
 
         f.close()
 

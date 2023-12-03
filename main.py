ac9655927d7b5d7da14a8363c14b1d692a31461e
Yuushi000 <yuushi.berly@efrei.net>
2023-12-03 20:05:47 +0100

    pour n'ettoyer les fichiers

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 import os
 
 
-def propre(repertoire):
-    fichiers = []
-    for fichier in os.listdir(repertoire):
-        if fichier.endswith(".txt"):
-            fichiers.append(fichier)
-###crée une fonction qui permettra de lire directement a partir des fichier
-    for j in range(len(fichiers)):
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    #crée une fonction qui permettra de lire directement a partir des fichiers
+    return files_names
 
-        txt1 = []
 
-        file = open("{}\\{}".format(repertoire, fichiers[j]), "r", encoding="utf8")
-        lines = file.readlines()
-        for n, line in enumerate(lines):
-            txt1.append(str(line).replace("\n", " "))
-        file.close()
-        for i in range(len(L1)):
-            txt1[i] = txt1[i].replace(".", " ")
-            txt1[i] = txt1[i].replace(",", "")
-            txt1[i] = txt1[i].replace("'", "")
-            txt1[i] = txt1[i].replace(";", "")
-            txt1[i] = txt1[i].replace("!", "")
-            txt1[i] = txt1[i].replace("?", "")
-            txt1[i] = txt1[i].replace("-", " ")
-            txt1[i] = txt1[i].lower()
-###permet de remplacer tout les points, virgule etc par des espaces et de tout mettre en minuscule
-        f = open("{}\\{}".format(repertoire, fichiers[j]), "w")
-        for n, line in enumerate(lines):
-            f.write("{}\n".format(txt1[n]))
 
-        f.close()
-###permet aussi d'écrire dans les fichiers
+directory = "./speeches"
+files_names = list_of_files(directory, "txt")
+print_list(files_names)
 
-propre("speeches")
+for i in range(len(files_names)):
+    files_names[i] = files_names[i].replace(".", "")
+    files_names[i] = files_names[i].replace(",", "")
+    files_names[i] = files_names[i].replace("'", " ")
+    files_names[i] = files_names[i].replace(";", "")
+    files_names[i] = files_names[i].replace("!", "")
+    files_names[i] = files_names[i].replace("?", "")
+    files_names[i] = files_names[i].replace("-", " ")
+    files_names[i] = files_names[i].lower()
+#permet de remplacer tout les points, virgule etc par des espaces
+# et de tout mettre en minuscule pour chaque fichier
